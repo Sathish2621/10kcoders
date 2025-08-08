3f2a30efbbd64b7debf4e068729a2f083025d159
kunapureddy satish <sathishkumapureddy@gmail.com>
2025-08-08 23:21:51 +0530

    javascript

--- user_verification/script.js
+++ user_verification/script.js
@@ -1,13 +1,19 @@
 document.getElementById("heading").style.color="orange"
 
+function verifying(){
+    document.getElementById("btn").innerText = "Your account is verifying...";
+}
+
 function verifying() {
     let username = document.getElementById("name").value;
     let password = document.getElementById("password").value;
-    if (username === "sathish" && password === "1234") {
+    if (username == "sathish" && password == "1234") {
         document.getElementById("heading").innerText = "Login Successful ✅";
         document.getElementById("heading").style.color = "green";
+        document.getElementById("status").innerText = "Your account is Login sucessfully..";
     } else {
         document.getElementById("heading").innerText = "Invalid Credentials ❌";
         document.getElementById("heading").style.color = "red";
+        document.getElementById("status").innerText = "Your account is verifying...";
     }
 }
